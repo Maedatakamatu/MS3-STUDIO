a146bbb203a2e4e73f202cd00b394ce09535733c
sembo yusuke <semboyusuke@sembonoMacBook-Air.local>
2020-12-19 20:51:56 +0900

    page-home.php追加

---
+++ wp-content/themes/MS3-STUDIO/page-home.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+
+    <div class="home_container">
+        <div class="home_content">
+            <div class="home_topic">
+                <?php echo do_shortcode('[bbp-topic-index]'); ?>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer();?>

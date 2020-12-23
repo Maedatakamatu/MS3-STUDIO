81a1004fad58420df2088ec781d0d05fe9f19c66
Nagao Sora <ziyuu1014@gmail.com>
2020-12-23 21:12:11 +0900

    CSSやりました

--- wp-content/themes/MS3-STUDIO/single.php
+++ wp-content/themes/MS3-STUDIO/single.php
@@ -1,18 +1,20 @@
 <?php get_header();?>
-<br>
-<br>
-<br>
-<br>
-        <?php the_post(); ?>
-        <div class="blog-content">
-            <?php the_post_thumbnail();  ?>
-            <div class="blog-info">
-                <h2><?php the_title(); ?></h2>
-                <p>今日の天気は<?php the_field('weather') ;?>です</p>
-                <p>今日の会社の様子：<?php the_field('report') ;?></p>
-                <img src="<?php the_field('office_image') ;?>" alt="会社の様子の画像">
-                <p><?php the_content(); ?></p>
-                <a href="#"><i class="fas fa-globe-americas"></i><?php the_category(); ?></a><a href="#"><i class="fas fa-globe-americas"></i><?php the_time('Y年n月j日') ?></a>
+    <div class="home">
+        <h1>投稿一覧</h1>
+        <div class="home_container">
+            <div class="sidebar"><!--サイドバーを表示するエリア flexの親要素-->         
+                <div class="sidebar_menu"><!--サイドバー flexの子要素-->
+                    <?php get_sidebar();?>
+                </div>
+            </div><!--サイドバーが表示されるエリアの閉じタグ flexの親要素-->
+            
+            <div class="home_content">
+                <div class="home_topic">
+                    <?php comments_template(); ?>
+                </div>
             </div>
+
         </div>
+
+    </div>
 <?php get_footer();?>
